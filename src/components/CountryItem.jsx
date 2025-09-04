0b18c572bda56388a42018921af74e2b6c64a3ea
Maciej <wielgosz.mac@gmail.com>
2025-09-04 17:31:47 +0200

    first commit

---
+++ src/components/CountryItem.jsx
@@ -0,0 +1,17 @@
+import styles from "./CountryItem.module.css";
+
+function CountryItem({ country }) {
+  return (
+    <li className={styles.countryItem}>
+      <span>
+        <img
+          src={`https://flagcdn.com/24x18/${country.emoji.toLowerCase()}.png`}
+          alt="flag"
+        />
+      </span>
+      <span>{country.country}</span>
+    </li>
+  );
+}
+
+export default CountryItem;
